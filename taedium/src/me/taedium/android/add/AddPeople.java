dd05216db3e039c756f605aa3448df46ce483dbf
Andrew Halberstadt <halbersa@gmail.com>
2011-11-20 11:40:55 -0500

    Fix bug where we were still defaulting to 1000 for max people

--- taedium/src/me/taedium/android/add/AddPeople.java
+++ taedium/src/me/taedium/android/add/AddPeople.java
@@ -36,8 +36,8 @@ public class AddPeople extends WizardActivity {
     protected void fillData() {
     	String min_people = etMinPeople.getText().toString();
     	String max_people = etMaxPeople.getText().toString();
-    	if (min_people == "") min_people = "1";
-    	if (max_people == "") max_people = "1";
+    	if (min_people.equals("")) min_people = "1";
+    	if (max_people.equals("")) max_people = "1";
     	data.putString("min_people", escapeString(min_people));
     	data.putString("max_people", escapeString(max_people));
         data.putInt("ages", rgAges.getCheckedRadioButtonId());
